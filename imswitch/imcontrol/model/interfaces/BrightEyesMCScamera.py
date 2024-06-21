3d9e0644d346ddd51d778dc39c05f9fe4f2e7f86
Mattia Donato <mattia.donato@iit.it>
2024-06-21 14:01:31 +0200

    Add the first for controlling BrightEyes-MCS with REST API

--- imswitch/imcontrol/model/interfaces/BrightEyesMCScamera.py
+++ imswitch/imcontrol/model/interfaces/BrightEyesMCScamera.py
@@ -9,146 +9,225 @@ import cv2
 import numpy as np
 from threading import Thread
 import urllib
-import requests
 import json
-import numpy as np
 from io import BytesIO
 
 
 class BrightEyesMCSCamera:
     def __init__(self, host, port):
+        """
+        Initializes the BrightEyesMCSCamera object with the given host and port.
+
+        Parameters:
+        host (str): The host address of the camera.
+        port (int): The port number of the camera.
+        """
         super().__init__()
         self.__logger = initLogger(self, tryInheritParent=True)
-        # URL for the remote camera
         self.host = host
         self.port = port
 
-        # many to be purged
+        # Camera parameters and properties
         self.model = "BrightEyesMCSCamera"
         self.shape = (0, 0)
-
-        # camera parameters
         self.framesize = 100
         self.exposure_time = 0
         self.analog_gain = 0
-
         self.internal_property = {
-             "image_width" : 2000,
-             "image_height" : 2000
+            "image_width": 2000,
+            "image_height": 2000
         }
-        #%% starting the camera thread
 
+        # Initialize camera network interface
         self.camera = BrightEyesMCSNetwork(self.host, self.port, is_debug=True)
-
         self.frame = np.zeros((self.internal_property["image_height"], self.internal_property["image_width"]))
-        #TODO: here everything is a to-do... many things to remove and correct
+
     def put_frame(self, frame):
+        """
+        Updates the current frame with the given frame and resizes it to the camera's image dimensions.
+
+        Parameters:
+        frame (numpy.ndarray): The frame to update.
+        """
         self.frame = frame
         self.frame = cv2.resize(self.frame, (self.internal_property["image_width"], self.internal_property["image_height"]))
         return frame
 
     def start_live(self):
-        # most likely the camera already runs
+        """
+        Sends a command to start the live preview on the camera.
+        """
         self.camera.send_command("preview")
 
     def stop_live(self):
+        """
+        Sends a command to stop the live preview on the camera.
+        """
         self.camera.send_command("stop")
 
     def suspend_live(self):
+        """
+        Sends a command to stop the live preview on the camera (alias for stop_live).
+        """
         self.camera.send_command("stop")
 
     def prepare_live(self):
+        """
+        Placeholder for preparing the live view (not implemented).
+        """
         pass
 
     def close(self):
-        pass  #TODO: self.camera.close()
+        """
+        Placeholder for closing the camera connection (not implemented).
+        """
+        pass
 
     def set_exposure_time(self, exposure_time):
+        """
+        Sets the exposure time of the camera.
+
+        Parameters:
+        exposure_time (int): The desired exposure time.
+        """
         self.exposure_time = exposure_time
         self.camera.setExposureTime(self.exposure_time)
 
     def set_analog_gain(self, analog_gain):
+        """
+        Sets the analog gain of the camera.
+
+        Parameters:
+        analog_gain (int): The desired analog gain.
+        """
         self.analog_gain = analog_gain
         self.camera.setGain(self.analog_gain)
 
     def set_framesize(self, framesize):
+        """
+        Sets the frame size of the camera.
+
+        Parameters:
+        framesize (int): The desired frame size.
+        """
         self.framesize = framesize
         self.camera.setFrameSize(self.framesize)
 
-    def set_ledIntensity(self, ledIntensity):
-        self.ledIntensity = ledIntensity
-        self.camera.setLed(self.ledIntensity)
-
-    def set_pixel_format(self, format):
-        pass
-
     def getLast(self):
-        # get frame and save
-        self.frame =  self.camera.download_array("preview")
+        """
+        Retrieves the last frame from the camera's preview.
+
+        Returns:
+        numpy.ndarray: The last frame.
+        """
+        self.frame = self.camera.download_array("preview")
         return self.frame
 
     def getLastChunk(self):
-        return  self.camera.download_array("preview")
+        """
+        Retrieves the last chunk of data from the camera's preview.
+
+        Returns:
+        numpy.ndarray: The last chunk of data.
+        """
+        return self.camera.download_array("preview")
 
     def setROI(self, hpos, vpos, hsize, vsize):
+        """
+        Sets the Region of Interest (ROI) for the camera.
+
+        Parameters:
+        hpos (int): The horizontal position of the ROI.
+        vpos (int): The vertical position of the ROI.
+        hsize (int): The horizontal size of the ROI.
+        vsize (int): The vertical size of the ROI.
+        """
         hsize = max(hsize, 256)  # minimum ROI size
         vsize = max(vsize, 24)  # minimum ROI size
-        pass
-        # self.__logger.debug(
-        #     f'{self.model}: setROI started with {hsize}x{vsize} at {hpos},{vpos}.'
-        # )
-        #self.camera.setROI(vpos, hpos, vsize, hsize)
 
     def setPropertyValue(self, property_name, property_value):
+        """
+        Sets the property value for the camera.
+
+        Parameters:
+        property_name (str): The name of the property.
+        property_value (any): The value of the property.
+
+        Returns:
+        any: The set property value.
+        """
         self.camera.set_gui_data({property_name: property_value})
-        # self.__logger.warning(f'Property {property_name} does not exist')
-        # return False
         return property_value
 
     def getPropertyValue(self, property_name):
+        """
+        Retrieves the property value for the given property name.
+
+        Parameters:
+        property_name (str): The name of the property.
+
+        Returns:
+        any: The value of the property.
+        """
         if property_name in self.internal_property:
             return self.internal_property[property_name]
         else:
             try:
                 property_value = self.camera.get_gui_data(property_name)
-            except e as Exception:
+            except Exception as e:
                 print(e)
-        # Check if the property exists.
-        # if property_name == "gain":
-        #     property_value = self.camera.gain
-        # elif property_name == "exposure":
-        #     property_value = self.camera.exposuretime
-        # elif property_name == "led":
-        #     property_value = self.camera.exposuretime
-        # elif property_name == "framesize":
-        #     property_value = self.camera.framesize
-        # elif property_name == "image_width":
-        #     property_value = self.camera.SensorWidth
-        # elif property_name == "image_height":
-        #     property_value = self.camera.SensorHeight
-        # else:
-        #     self.__logger.warning(f'Property {property_name} does not exist')
-        #     return False
         return property_value
 
     def openPropertiesGUI(self):
+        """
+        Placeholder for opening the properties GUI (not implemented).
+        """
         pass
 
 
 class BrightEyesMCSNetwork(object):
 
     def __init__(self, host="127.0.0.1", port=8000, is_debug=True):
+        """
+        Initializes the BrightEyesMCSNetwork object with the given host, port, and debug flag.
+
+        Parameters:
+        host (str): The host address of the network.
+        port (int): The port number of the network.
+        is_debug (bool): Flag indicating if debug mode is enabled.
+        """
         self.base_url = f"http://{host}:{port}"
         self.debug = is_debug
 
     def get_param(self, param):
+        """
+        Retrieves the parameter from the GUI data.
+
+        Parameters:
+        param (str): The parameter to retrieve.
+        """
         self.get_gui_data()
 
     def get_root(self):
+        """
+        Retrieves the root information from the server.
+
+        Returns:
+        dict: The root information.
+        """
         response = requests.get(f"{self.base_url}/")
         return response.json()
 
     def get_gui_data(self, item=None):
+        """
+        Retrieves the GUI data from the server.
+
+        Parameters:
+        item (str, optional): Specific item to retrieve from the GUI data.
+
+        Returns:
+        dict: The GUI data.
+        """
         url = f"{self.base_url}/gui/"
         if item:
             url += f"{item}"
@@ -156,19 +235,48 @@ class BrightEyesMCSNetwork(object):
         return json.loads(response.json())
 
     def send_command(self, command):
+        """
+        Sends a command to the server.
+
+        Parameters:
+        command (str): The command to send.
+
+        Returns:
+        str: The response from the server.
+        """
         url = f"{self.base_url}/cmd/{command}"
         response = requests.get(url)
         return response.text
 
     def set_gui_data(self, data):
+        """
+        Sets the GUI data on the server.
+
+        Parameters:
+        data (dict): The data to set.
+
+        Returns:
+        str: The response from the server.
+        """
         url = f"{self.base_url}/set"
         headers = {"Content-Type": "application/json"}
-        d=json.dumps(data)
+        d = json.dumps(data)
         print("data=json.dumps(data)", d)
         response = requests.put(url, data=d, headers=headers)
         return response.text
 
     def upload_array(self, array, shape, dtype="float64"):
+        """
+        Uploads an array to the server.
+
+        Parameters:
+        array (numpy.ndarray): The array to upload.
+        shape (tuple): The shape of the array.
+        dtype (str, optional): The data type of the array (default is "float64").
+
+        Returns:
+        dict: The response from the server.
+        """
         url = f"{self.base_url}/array/"
         files = {'file': ('array', array.tobytes(), 'application/octet-stream')}
         data = {'shape': ','.join(map(str, shape)), 'dtype': dtype}
@@ -176,6 +284,18 @@ class BrightEyesMCSNetwork(object):
         return response.json()
 
     def download_image(self, image_type):
+        """
+        Downloads an image from the server.
+
+        Parameters:
+        image_type (str): The type of image to download ("preview" or "fingerprint").
+
+        Returns:
+        bytes: The downloaded image content.
+
+        Raises:
+        ValueError: If the image type is invalid.
+        """
         if image_type not in ["preview", "fingerprint"]:
             raise ValueError("Invalid image type. Must be 'preview' or 'fingerprint'.")
         url = f"{self.base_url}/{image_type}.png"
@@ -186,6 +306,18 @@ class BrightEyesMCSNetwork(object):
             response.raise_for_status()
 
     def download_array(self, array_type):
+        """
+        Downloads an array from the server.
+
+        Parameters:
+        array_type (str): The type of array to download ("preview" or "fingerprint").
+
+        Returns:
+        numpy.ndarray: The downloaded array.
+
+        Raises:
+        ValueError: If the array type is invalid.
+        """
         if array_type not in ["preview", "fingerprint"]:
             raise ValueError("Invalid array type. Must be 'preview' or 'fingerprint'.")
         url = f"{self.base_url}/{array_type}.np"
@@ -199,20 +331,6 @@ class BrightEyesMCSNetwork(object):
             return np_array
         else:
             response.raise_for_status()
-
-# Example usage:
-# client = FastAPIClient()
-# print(client.get_root())
-# print(client.get_gui_data('all'))
-# client.send_command('preview')
-# client.set_gui_data({"key": "value"})
-# array = np.array([[1, 2], [3, 4]], dtype="float64")
-# client.upload_array(array, array.shape, dtype=array.dtype.name)
-# image_data = client.download_image('preview')
-# with open("preview.png", "wb") as f:
-#     f.write(image_data)
-# array_data = client.download_array('preview')
-# print(array_data)
 
 
 # Copyright (C) ImSwitch developers 2021
--- imswitch/imcontrol/model/managers/detectors/BrightEyesMCSManager.py
+++ imswitch/imcontrol/model/managers/detectors/BrightEyesMCSManager.py
@@ -1,60 +1,28 @@
 from typing import List, Tuple, Optional, Dict
-
 import numpy as np
-
 from imswitch.imcommon.model import initLogger
-#from imswitch.imcontrol.model.interfaces.BrightEyesMCScamera import BrightEyesMCSCamera
-from .DetectorManager import (DetectorManager, DetectorAction, DetectorNumberParameter,
-                              DetectorParameter, DetectorBooleanParameter, DetectorListParameter)
-
-
-# class BrightEyesMCSManager(DetectorManager):
-#
-#     def __init__(self, detectorInfo, name, **_lowLevelManagers):
-#         self.__logger = initLogger(self, instanceName=name)
-#         self._camera = self._getBrightEyesMCSobj(host, port)
-#         pass
-#
-#     @property
-#     def pixelSizeUm(self) -> List[int]:
-#         pass
-#
-#     def crop(self, hpos: int, vpos: int, hsize: int, vsize: int) -> None:
-#         pass
-#
-#     def getLatestFrame(self) -> np.ndarray:
-#         pass
-#
-#     def getChunk(self) -> np.ndarray:
-#         pass
-#
-#     def flushBuffers(self) -> None:
-#         pass
-#
-#     def startAcquisition(self) -> None:
-#         pass
-#
-#     def stopAcquisition(self) -> None:
-#         pass
-
+from .DetectorManager import (
+    DetectorManager, DetectorAction, DetectorNumberParameter,
+    DetectorParameter, DetectorBooleanParameter, DetectorListParameter
+)
 
 class BrightEyesMCSManager(DetectorManager):
-    """ DetectorManager that deals with TheImagingSource cameras and the
-    parameters for frame extraction from them.
-
-    Manager properties:
-
-    - ``cameraListIndex`` -- the camera's index in the Allied Vision camera list (list
-      indexing starts at 0); set this string to an invalid value, e.g. the
-      string "mock" to load a mocker
-    - ``picamera`` -- dictionary of Allied Vision camera properties
+    """
+    Manages the BrightEyesMCS camera system.
     """
 
     def __init__(self, detectorInfo, name, **_lowLevelManagers):
+        """
+        Initializes the BrightEyesMCSManager object.
+
+        Parameters:
+        detectorInfo: Information about the detector.
+        name (str): The name of the manager.
+        _lowLevelManagers: Additional low-level managers.
+        """
         self.__logger = initLogger(self, instanceName=name)
 
-        # TODO: here everything is a to-do... many things to remove and correct
-
+        # Extract host and port information from detector properties
         host = detectorInfo.managerProperties['cameraHost']
         port = detectorInfo.managerProperties['cameraPort']
 
@@ -70,8 +38,6 @@ class BrightEyesMCSManager(DetectorManager):
 
         fullShape = (self._camera.getPropertyValue('image_width'),
                      self._camera.getPropertyValue('image_height'))
-
-
 
         self.crop(hpos=0, vpos=0, hsize=fullShape[0], vsize=fullShape[1])
 
@@ -94,54 +60,105 @@ class BrightEyesMCSManager(DetectorManager):
             'calib_x': DetectorNumberParameter(group='Misc', value=8.89999, valueUnits='um/V', editable=True),
             'calib_y': DetectorNumberParameter(group='Misc', value=8.89999, valueUnits='um/V', editable=True),
             'calib_z': DetectorNumberParameter(group='Misc', value=10, valueUnits='um/V', editable=True),
-
             'preview_autoscale': DetectorBooleanParameter(group='Misc', value=True, editable=True),
-
             'projection': DetectorListParameter(group='Misc', value="xy", options=["xy","yx","zx"], editable=True),
             'preview_channel': DetectorListParameter(group='Misc', value="Sum", options=["Sum", "1", "12", "24"], editable=True),
-
             'fingerprint_visualization': DetectorListParameter(group='Misc', value="Cumulative", options=["Cumulative"], editable=True),
-
             'fingerprint_autoscale': DetectorBooleanParameter(group='Misc', value=True, editable=True),
             'ratio_xy_locked': DetectorBooleanParameter(group='Misc', value=True, editable=True),
             'waitOnlyFirstTime': DetectorBooleanParameter(group='Misc', value=False, editable=True),
-
             'waitAfterFrame': DetectorNumberParameter(group='Misc', value=0.0, valueUnits='um', editable=True),
-
             'laserOffAfterMeas': DetectorBooleanParameter(group='Misc', value=False, editable=True),
-
             'ch_preview': DetectorListParameter(group='Misc', value="Sum", options=["Sum", "1", "12", "24"], editable=True),
-
-
-
-            # "\"defaultFolder\": \"C:/Users/Developer/Documents/Data\", "
-            # "\"spad_number_of_channels\": \"25\","
-            # " \"comment\":"
-            # " \"\", \"bitFile\": \"bitfiles/SingleBoard-USB7856R.lvbitx\""
-
-            }
+        }
 
         # Prepare actions
         actions = {
-            'More properties': DetectorAction(group='Misc',
-                                              func=self._camera.openPropertiesGUI)
+            'More properties': DetectorAction(group='Misc', func=self._camera.openPropertiesGUI),
+            'Force STOP': DetectorAction(group='Misc', func=self._camera.stop_live)
         }
 
         super().__init__(detectorInfo, name, fullShape=fullShape, supportedBinnings=[1],
                          model=model, parameters=parameters, actions=actions, croppable=True)
 
-    def getLatestFrame(self, is_save=False):
-        if is_save:
-            return self._camera.getLastChunk()
-        else:
-            return self._camera.getLast()
+    @property
+    def pixelSizeUm(self) -> List[int]:
+        """
+        Returns the pixel size in micrometers.
+
+        Returns:
+        List[int]: A list of pixel sizes in micrometers.
+        """
+        return [1, 1, 1]
+
+    def crop(self, hpos: int, vpos: int, hsize: int, vsize: int) -> None:
+        """
+        Crops the camera frame.
+
+        Parameters:
+        hpos (int): Horizontal position of the crop.
+        vpos (int): Vertical position of the crop.
+        hsize (int): Horizontal size of the crop.
+        vsize (int): Vertical size of the crop.
+        """
+        pass
+
+    def getLatestFrame(self) -> np.ndarray:
+        """
+        Retrieves the latest frame from the camera.
+
+        Returns:
+        numpy.ndarray: The latest frame.
+        """
+        return self._camera.getLast()
+
+    def getChunk(self):
+        """
+        Retrieves the latest chunk of data from the camera.
+
+        Returns:
+        numpy.ndarray: The latest chunk of data.
+        """
+        return self._camera.getLastChunk()
+
+    def flushBuffers(self) -> None:
+        """
+        Flushes the camera buffers.
+        """
+        pass
+
+    def startAcquisition(self) -> None:
+        """
+        Starts data acquisition from the camera.
+        """
+        if not self._running:
+            self._camera.start_live()
+            self._running = True
+            self.__logger.debug('startlive')
+
+    def stopAcquisition(self) -> None:
+        """
+        Stops data acquisition from the camera.
+        """
+        if self._running:
+            self._running = False
+            self._camera.suspend_live()
+            self.__logger.debug('suspendlive')
 
     def setParameter(self, name, value):
-        """Sets a parameter value and returns the value.
-        If the parameter doesn't exist, i.e. the parameters field doesn't
-        contain a key with the specified parameter name, an error will be
-        raised."""
+        """
+        Sets a parameter value and returns the value.
 
+        Parameters:
+        name (str): The name of the parameter.
+        value (any): The value to set.
+
+        Returns:
+        any: The set parameter value.
+
+        Raises:
+        AttributeError: If the parameter does not exist.
+        """
         super().setParameter(name, value)
 
         if name not in self._DetectorManager__parameters:
@@ -151,99 +168,45 @@ class BrightEyesMCSManager(DetectorManager):
         return value
 
     def getParameter(self, name):
-        """Gets a parameter value and returns the value.
-        If the parameter doesn't exist, i.e. the parameters field doesn't
-        contain a key with the specified parameter name, an error will be
-        raised."""
+        """
+        Gets a parameter value and returns the value.
 
+        Parameters:
+        name (str): The name of the parameter.
+
+        Returns:
+        any: The parameter value.
+
+        Raises:
+        AttributeError: If the parameter does not exist.
+        """
         if name not in self._parameters:
             raise AttributeError(f'Non-existent parameter "{name}" specified')
 
-        value = self._camera.getPropertyValue(name)
-        return value
-
-    def setBinning(self, binning):
-        super().setBinning(binning) 
-        
-
-    def getChunk(self):
-        return self._camera.getLastChunk()
-
-    def flushBuffers(self):
-        pass
-
-    def startAcquisition(self):
-        if not self._running:
-            self._camera.start_live()
-            self._running = True
-            self.__logger.debug('startlive')
-
-    def stopAcquisition(self):
-        if self._running:
-            self._running = False
-            self._camera.suspend_live()
-            self.__logger.debug('suspendlive')
-
     def stopAcquisitionForROIChange(self):
+        """
+        Stops data acquisition to change the region of interest (ROI).
+        """
         self._running = False
         self._camera.stop_live()
         self.__logger.debug('stoplive')
 
     def finalize(self) -> None:
+        """
+        Finalizes and safely disconnects the camera.
+        """
         super().finalize()
         self.__logger.debug('Safely disconnecting the camera...')
         self._camera.close()
 
-    @property
-    def pixelSizeUm(self):
-        return [1, 1, 1]
-
-    def crop(self, hpos, vpos, hsize, vsize):
-        if(0):
-            def cropAction():
-                # self.__logger.debug(
-                #     f'{self._camera.model}: crop frame to {hsize}x{vsize} at {hpos},{vpos}.'
-                # )
-                self._camera.setROI(hpos, vpos, hsize, vsize)
-
-            self._performSafeCameraAction(cropAction)
-            # TODO: unsure if frameStart is needed? Try without.
-            # This should be the only place where self.frameStart is changed
-            self._frameStart = (hpos, vpos)
-            # Only place self.shapes is changed
-            self._shape = (hsize, vsize)
-        # TODO: Reimplement
-
-    def _performSafeCameraAction(self, function):
-        """ This method is used to change those camera properties that need
-        the camera to be idle to be able to be adjusted.
-        """
-        self._adjustingParameters = True
-        wasrunning = self._running
-        self.stopAcquisitionForROIChange()
-        function()
-        if wasrunning:
-            self.startAcquisition()
-        self._adjustingParameters = False
-
     def openPropertiesDialog(self):
+        """
+        Opens the properties dialog for the camera.
+        """
         self._camera.openPropertiesGUI()
 
     def closeEvent(self):
+        """
+        Closes the camera connection.
+        """
         self._camera.close()
-
-# Copyright (C) ImSwitch developers 2021
-# This file is part of ImSwitch.
-#
-# ImSwitch is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ImSwitch is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
